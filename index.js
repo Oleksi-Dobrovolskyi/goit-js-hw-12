d14321a0b12c09f5b3794cd95e3b0de035ef6ebb
Oleksi-Dobrovolskyi <alex.puhovski@gmail.com>
2025-01-27 07:07:45 +0000

    Deploying to gh-pages from @ Oleksi-Dobrovolskyi/goit-js-hw-12@ac4f56fc9b369beab41d8c1f8b89896a29ec3471 🚀

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-import{S as u,i as a}from"./assets/vendor-5ObWk2rO.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const f="48313222-9e5699caa5ef89de4c12bc71b",p="https://pixabay.com/api/",m=document.querySelector("#search-form"),i=document.querySelector("#gallery"),l=document.querySelector("#loader");let d=new u(".gallery a",{captionsData:"alt",captionDelay:250});m.addEventListener("submit",s=>{s.preventDefault();const o=s.target.searchQuery.value.trim();if(!o){a.error({title:"Error",message:"Please enter a search query!"});return}y(o)});async function y(s){g();try{const r=await(await fetch(`${p}?key=${f}&q=${encodeURIComponent(s)}&image_type=photo&orientation=horizontal&safesearch=true`)).json();if(r.hits.length===0){a.warning({title:"No Results",message:"Sorry, there are no images matching your search query. Please try again!"}),i.innerHTML="";return}h(r.hits),d.refresh()}catch{a.error({title:"Error",message:"Failed to fetch images. Please try again later."})}finally{b()}}function h(s){i.innerHTML="";const o=s.map(r=>`
-      <a href="${r.largeImageURL}" class="gallery-item">
-        <img src="${r.webformatURL}" alt="${r.tags}" loading="lazy" />
+import{S as y,i as d,a as g}from"./assets/vendor-DEenWwFD.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&s(l)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const h="48313222-9e5699caa5ef89de4c12bc71b",b="https://pixabay.com/api/",L=document.querySelector("#search-form"),u=document.querySelector("#gallery"),m=document.querySelector("#loader"),f=document.querySelector("#pagination");let n=1,i="",c=0,v=new y(".gallery a",{captionsData:"alt",captionDelay:250});L.addEventListener("submit",o=>{if(o.preventDefault(),i=o.target.searchQuery.value.trim(),!i){d.error({title:"Error",message:"Please enter a search query!"});return}n=1,p(i,n)});async function p(o,a){q();try{const s=(await g.get(b,{params:{key:h,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15,page:a}})).data;if(s.hits.length===0){d.warning({title:"No Results",message:"Sorry, there are no images matching your search query. Please try again!"}),u.innerHTML="",f.innerHTML="";return}c=Math.ceil(s.totalHits/10),P(s.hits),$()}catch{d.error({title:"Error",message:"Failed to fetch images. Please try again later."})}finally{S()}}function P(o){u.innerHTML="";const a=o.map(t=>`
+      <a href="${t.largeImageURL}" class="gallery-item">
+        <img src="${t.webformatURL}" alt="${t.tags}" loading="lazy" />
         <div class="gallery-info">
           <div>
-            <p><b>Likes:</b> ${r.likes}</p>
-            <p><b>Views:</b> ${r.views}</p>
+            <p><b>Likes:</b> ${t.likes}</p>
+            <p><b>Views:</b> ${t.views}</p>
           </div>
           <div>
-            <p><b>Comments:</b> ${r.comments}</p>
-            <p><b>Downloads:</b> ${r.downloads}</p>
+            <p><b>Comments:</b> ${t.comments}</p>
+            <p><b>Downloads:</b> ${t.downloads}</p>
           </div>
         </div>
       </a>
-    `).join("");i.innerHTML=o,d.refresh()}function g(){l.classList.remove("hidden")}function b(){l.classList.add("hidden")}
+    `).join("");u.innerHTML=a,v.refresh()}function $(){f.innerHTML="";const o=`
+    <button class="btn-prev" ${n===1?"disabled":""}>Previous</button>
+  `,a=`
+    <button class="btn-next" ${n===c?"disabled":""}>Next</button>
+  `;f.innerHTML=`${o} <span>Page ${n} of ${c}</span> ${a}`;const t=document.querySelector(".btn-prev"),s=document.querySelector(".btn-next");t&&t.addEventListener("click",()=>{n>1&&(n--,p(i,n))}),s&&s.addEventListener("click",()=>{n<c&&(n++,p(i,n))})}function q(){m.classList.remove("hidden")}function S(){m.classList.add("hidden")}
 //# sourceMappingURL=index.js.map
